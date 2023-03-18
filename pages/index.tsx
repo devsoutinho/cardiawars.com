c41f7b314508f6fd81c06fced97795aa66aa7f3b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-03-18 18:39:59 -0300

    update

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>CardIA Wars</h1>
+
+      <ul>
+        <li>
+          #01 Cartinha Show 
+        </li>
+      </ul>
+    </div>
+  )
+}
